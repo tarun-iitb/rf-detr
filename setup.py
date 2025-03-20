b69f234d514ef5f43c6053fab4ba93134cf771de
Peter Robicheaux <peter@roboflow.com>
2025-03-20 14:30:42 -0700

    Grad accum fix 2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setup(
     name="rfdetr",
-    version="1.0.5",
+    version="1.0.6",
     description="RF-DETR",
     author="Roboflow, Inc",
     author_email="peter@roboflow.com",
