5b08efc02a41de21081dc3158c21c69e4cc20f28
Peter Robicheaux <peter@roboflow.com>
2025-03-21 03:16:41 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setup(
     name="rfdetr",
-    version="1.0.6",
+    version="1.0.7",
     description="RF-DETR",
     author="Roboflow, Inc",
     author_email="peter@roboflow.com",
